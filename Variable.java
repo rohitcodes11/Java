ea5cdfafe512476e3dccf20226031368261b23c7
rohitcodes11 <rkumarjha134@gmail.com>
2026-06-22 17:32:26 +0530

    Varibles and data types program..

---
+++ Variable.java
@@ -0,0 +1,19 @@
+public class Variable {
+    public static void main(String[]args){
+        int marks = 85;
+        String name = "Ram";
+        char grade = 'A';
+        boolean pass = true;
+        double percentage = 85.8;
+
+        System.out.println(marks);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(pass);
+        System.out.println(percentage);
+        
+    
+    }
+    
+    
+}

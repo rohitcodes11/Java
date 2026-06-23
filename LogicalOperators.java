e02136ece882042b13987818e30985859f02f022
rohitcodes11 <rkumarjha134@gmail.com>
2026-06-23 22:34:17 +0530

    06.LogicalOperators
     1️⃣ AND (&&)
    Dono conditions true honi chahiye.
    2️⃣ OR (||)
    Ek bhi condition true ho to result true.
    3️⃣ NOT (!)
    Boolean value ko ulta kar deta hai.
    
    true && true   = true
    true && false  = false
    true || false  = true
    false || false = false
    
    ogical operators multiple conditions ko combine karne ke liye use hote hain. Java me main logical operators && (AND), || (OR), aur ! (NOT) hain. Inka result hamesha boolean (true ya false) hota hai. Ye operators mostly if, if-else, loops aur decision-making statements me use hote hain.../
    
    !true  = false
    !false = true

---
+++ LogicalOperators.java
@@ -0,0 +1,14 @@
+  // Logical Operators
+
+public class LogicalOperators {
+    public static void main (String[]args){
+        System.out.println(10 > 5 && 20 > 10);
+        System.out.println(10 > 5 && 20 < 10);
+
+        System.out.println(10 > 5 || 2 > 8);
+        System.out.println(3 > 8 || 1 > 5);
+
+        System.out.println(!(10 > 5));
+        System.out.println(!(5 > 10));
+    }
+}

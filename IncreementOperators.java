e545c2dc7937cfd374bf752f95be4db5f7bb23db
rohitcodes11 <rkumarjha134@gmail.com>
2026-06-23 23:27:18 +0530

    08.IncreementOperators
    a++  = Pehle Use, Phir Increase
    
    ++a  = Pehle Increase, Phir Use
    
    a--  = Pehle Use, Phir Decrease
    
    --a  = Pehle Decrease, Phir Use

---
+++ IncreementOperators.java
@@ -0,0 +1,32 @@
+/*public class IncreementOperators {
+    public static void main (String[]args){
+        int a= 5;
+        a++;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+
+    }
+    
+}*/
+
+// post incrementand decrement operator
+public class IncreementOperators{
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        System.out.println(a++);
+        System.out.println(++a);
+
+        int b = 5;
+
+        System.out.println(b--);
+        System.out.println(--b);
+    }
+}

158dc6a4c31c09b575d78985d573cccf358ebd90
rohitcodes11 <rkumarjha134@gmail.com>
2026-06-23 22:49:54 +0530

    07. assignmentOperators
    Assignment operators variable ki value ko update karne ke liye use hote hain.
    
    +=, -=, *=, /=, %= shorthand operators hain.

---
+++ AssignmentOperators.java
@@ -0,0 +1,46 @@
+public class AssignmentsOperators{
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        a += 5;
+        System.out.println(a);
+
+        b -= 3;
+        System.out.println(b);
+
+        a *= 2;
+        System.out.println(a);
+
+        a /= 4;
+        System.out.println(a);
+
+        a %= 3;
+        System.out.println(a);
+    }
+}
+
+public class AssignmentOperators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        a += b;
+        System.out.println(a);
+
+        a -= b;
+        System.out.println(a);
+
+        a *= b;
+        System.out.println(a);
+
+        a /= b;
+        System.out.println(a);
+
+        a %= b;
+        System.out.println(a);
+    }
+}
+    

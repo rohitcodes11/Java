91ebd7020039270c44ebd7018b9ab142f93de412
rohitcodes11 <rkumarjha134@gmail.com>
2026-06-23 17:35:35 +0530

    Operators ..>> arithmatic operators

---
+++ Operators.java
@@ -0,0 +1,33 @@
+// ARITHMATIC OPERATORS..>>addition,multiplication , substraction, division,modulus..
+
+ public class Operators {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 6;
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Remainder = " + (a % b));
+    }
+}
+
+
+// now start user se input lena 
+// then 
+/*import java.util.Scanner;
+public class Operators{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a = sc.nextInt();
+        System.out.println("enter second number");
+        int b = sc.nextInt();
+
+        System.out.println(a+b);
+
+}
+
+} */

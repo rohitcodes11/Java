cbe025c312aad83d6d87712b3f6fd10003078363
rohitcodes11 <rkumarjha134@gmail.com>
2026-07-01 15:44:27 +0530

    09. if,else...
    all conditional statement.. like, if, if-else, if-else-if, nested -if, switch statement,..

---
+++ Ifstatement.java
@@ -0,0 +1,134 @@
+//import java.util.Scanner;
+public class Ifstatement {
+    public static void main (String[]args){
+        //int marks = 40;
+//         if(marks >= 33){
+//             System.out.println("Pass");
+//         }
+//         if(marks <= 33){
+//             System.out.println("no criteria ");
+//         }
+// System.out.println("Result Declared");
+
+// now if - else state ment //
+
+// if (marks >= 40){
+//     System.out.println(" pass");
+// }
+// else{
+//     System.out.println("fail");
+// }
+
+// write a program iin java takes integer input from the user and checks whether the number is positive..
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enetr a number ");
+// int number = sc.nextInt();
+
+// if(number>= 0){
+//     System.out.println("positive number");
+
+// }
+// else{
+//     System.out.println("negative number");
+// }
+// sc.close();
+//  EVEN AND ODD PROGRAM.....
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter a number");
+// int number = sc.nextInt();
+// if(number %2 == 0){
+//     System.out.println("number is even");
+// }
+// else{
+//     System.out.println("number is odd");
+// }
+
+//write a java program tht takes age as checks whether the person is eligible to vote..
+
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter a number");
+// int age = sc.nextInt();
+// if(age>=18){
+//     System.out.println("eligible");
+// }
+// else{
+//     System.out.println("not eligible");
+// }
+
+// write a program java that likes two integers as input and prints which one is greater..
+
+// Scanner sc = new Scanner(System.in);
+//  System.out.println("enter a number");
+// int a = sc.nextInt();
+// System.out.println("enter a second number");
+// int b = sc.nextInt();
+// if(a>b){
+//     System.out.println(a + "is a greater");
+// }
+// else if(b>a) {
+//     System.out.println(b + "is a greater");
+// }
+// else {
+//     System.out.println("this is equal");
+// }
+
+// Write a java program to classify temperature into categoriies.
+//  Scanner sc = new Scanner(System.in);
+//  System.out.println("enter temperature");
+//  int temperature = sc.nextInt();
+//  if(temperature<10){
+
+//     System.out.println("wow,very cold");
+//  }
+// else if 
+//     }
+
+// age < 13 → Child
+// age >= 13 aur age < 18 → Teenager
+// age >= 18 aur age < 60 → Adult
+// age >= 60 → Senior Citizen
+
+// Scanner sc = new Scanner(System.in);
+// System.out.println("enter a number");
+//  int age = sc.nextInt();
+// if(age<13){
+//     System.out.println("thisd is a child");
+// }
+// else if (age<18){
+//     System.out.println("you are in teenager");
+// }
+// else if (age <60){
+//     System.out.println("you are in become adult phase");
+
+// }
+// else{
+//     System.out.println("you are a senior citizen");
+// }
+
+
+//  SWITCH STATEMENT..
+int number = 2;
+
+switch(number) {
+
+    case 1:
+        System.out.println("One");
+
+    case 2:
+        System.out.println("Two");
+
+    case 3:
+        System.out.println("Three");
+
+    default:
+        System.out.println("Invalid");
+}
+
+
+
+}
+
+
+
+}
+
